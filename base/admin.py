5f6793a1aad5555fd134a91a8b268e203766a7d7
mun-a <karnevall.13.13@gmail.com>
2021-11-01 08:52:57 +0600

    add CRUD functionality for the base application

---
+++ base/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+from .models import Room, Topic, Message
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'host', 'topic', 'updated', 'created')
+    list_display_links = ('pk', 'name')
+
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+    list_display_links = ('pk', 'name')
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'room', 'user', 'updated', 'created')
+    list_display_links = ('pk', 'room')
+
+
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Message, MessageAdmin)
