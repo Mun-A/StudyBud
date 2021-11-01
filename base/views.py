e26940f908541e59549e0c666086be7432954afb
mun-a <karnevall.13.13@gmail.com>
2021-11-01 14:11:57 +0600

    add search feature

--- base/views.py
+++ base/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )   # If q is empty then return all
+
+    topics = Topic.objects.all()
+    room_count = rooms.count()
 
     context = {
         'rooms': rooms,
+        'topics': topics,
+        'room_count': room_count,
     }
     return render(request, 'base/home.html', context)
 
